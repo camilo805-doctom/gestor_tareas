6cf0ba71cb738560d619435f5f4f3fb10169f5c5
Camilo <camilomunozb203@gmail.com>
2024-06-28 22:12:29 -0500

    finalizacion del error en la consulta

--- base_datos/llamados_funcion/aregar_tarea.py
+++ base_datos/llamados_funcion/aregar_tarea.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 def agregar_tarea():
-    with sqlite3.connect("C:/Users/POWER/gestor_tareas_2") as tarea:
+    with sqlite3.connect("C:/Users/POWER/gestor.db") as tarea:
         try:
             consulta_cursor = tarea.cursor()
             usuario_titulo = str(input("Ingrese el titulo de la tarea: "))
@@ -9,7 +9,7 @@ def agregar_tarea():
             fecha_vencimiento = str(input("ingrese la fecha de vencimiento (DD/MM/YY): "))
             prioridad_tarea = str(input("ingrese un nivel de prioridad: "))
             tarea_completada = int(input("ponga 0 si la tarea esta completada y 1 si no: "))
-            consulta_cursor.execute("INSERT INTO Agregar_tareas (Tareas) VALUES (?,?,?,?,?)",(usuario_titulo,contenido_tarea,fecha_vencimiento,prioridad_tarea,tarea_completada))
+            consulta_cursor.execute("INSERT INTO Agregar_tareas (Titulo,Descripcion,Fecha_vencimiento,Prioridad,Completado) VALUES (?,?,?,?,?)",(usuario_titulo,contenido_tarea,fecha_vencimiento,prioridad_tarea,tarea_completada))
             tarea.commit()
             print("Datos ingresados correctamente")
         except ValueError as error:
