2a096b4dd61b884e65e10178ecefbaa592fcd2e0
Camilo <camilomunozb203@gmail.com>
2024-06-30 18:31:00 -0500

    Nuevos comentarios

--- base_datos/llamados_funcion/completar_tareas.py
+++ base_datos/llamados_funcion/completar_tareas.py
@@ -1,13 +1,38 @@
+
+# impotamos "sqlite3" para manejar la base de datos
+
 import sqlite3
 
 def completar_tarea():
+
+    # Establecemos la conexión a la base de datos usando el contexto 'with' para asegurar el cierre correcto
+
     with sqlite3.connect("C:/Users/POWER/gestor.db") as completar:
         try:
+
+            # Obtenemos un cursor para ejecutar las consultas SQL
+
             consulta_cursor = completar.cursor()
+
+            # Solicitamos al usuario el título de la tarea que desea marcar como completada
+
             tarea_completada = str(input("Ingrese el titulo de la tarea completada: "))
+
+            # Verificamos si la tarea con el título proporcionado existe en la tabla 'Agregar_tarea'
+
             consulta_cursor.execute("SELECT * FROM Agregar_tarea WHERE Titulo = ?", (tarea_completada,))
+
+            # Actualizamos la tarea marcándola como completada (1)
+
             consulta_cursor.execute("UPDATE Agregar_tareas SET Completada = 1 Where Titulo = ?",(tarea_completada,))
+
+            # Confirmamos (commit) la transacción para guardar los cambios en la base de datos
+
             completar.commit()
             print("tarea marcada como completada")
+            
         except ValueError as error:
+
+            # Capturamos y manejamos cualquier excepción de tipo ValueError
+
             print(f"Error de digitacion: {error}")
